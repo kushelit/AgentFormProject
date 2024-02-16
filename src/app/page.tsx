3af13a2e206fd634efbfe4b4569a1339ee1c41a2
michal cohen <kushelit@gmail.com>
2024-02-16 12:51:05 +0200

    test2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import AgentForm from './AgentForm'; // Adjust the import path to where your Age
 const Page = () => {
   return (
     <div className="title-container">
-      <h1>מערכת לניהול מכירות סוכן</h1>
+      <h1>מערכת לניהול מכירות</h1>
       <AgentForm /> {/* This is where the AgentForm component is included */}
     </div>
   );
