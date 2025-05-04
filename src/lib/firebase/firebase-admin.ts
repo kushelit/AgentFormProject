dded64bd8ccec9748b32139dbd18b45eed9a2581
kusgelit <kuselit@gmail.com>
2025-05-04 21:10:32 +0300

    again test

--- src/lib/firebase/firebase-admin.ts
+++ src/lib/firebase/firebase-admin.ts
@@ -3,12 +3,12 @@ import admin from 'firebase-admin';
 if (!admin.apps.length) {
   let serviceAccount = {};
   try {
-    serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_KEY || '{}');
+    serviceAccount = JSON.parse(process.env.FIREBASE_ADMIN_KEY_JSON || '{}');
   } catch (e) {
-    console.error("❌ Failed to parse SERVICE_ACCOUNT_KEY:", e);
+    console.error("❌ Failed to parse FIREBASE_ADMIN_KEY_JSON:", e);
   } 
   // 🪵 הוסיפי כאן:
-console.log("🔍 Raw SERVICE_ACCOUNT_KEY env:", process.env.SERVICE_ACCOUNT_KEY);
+console.log("🔍 Raw SERVICE_ACCOUNT_KEY env:", process.env.FIREBASE_ADMIN_KEY_JSON);
 console.log("🔍 Parsed serviceAccount keys:", Object.keys(serviceAccount));
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
